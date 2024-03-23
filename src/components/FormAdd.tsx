35c6ad8f1d70a3b82d4e78ccc81eec655a25a370
DiegoASC287 <diego.armando020@hotmail.com>
2024-03-23 00:18:51 -0300

    Criando funcionalidades2

--- src/components/FormAdd.tsx
+++ src/components/FormAdd.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { IconX } from "@tabler/icons-react";
+import { useState } from "react";
 
 interface FormAddProps{
     atualizarClicou: () => void
@@ -7,10 +8,12 @@ interface FormAddProps{
 
 }
 export default function FormAdd({atualizarClicou, pedra}:FormAddProps){
+    const [nomeComprador, setNomeComprador] = useState<string>("")
     return <div className="absolute top-0 left-0 w-full h-full flex p-5">
     <div className="w-full h-full bg-zinc-200 flex p-2">
         <div className="flex-grow">
             <div className=" p-1">Pedra {pedra}</div>
+            <input type="text" className="bg-red-500" value={nomeComprador} onChange={e => setNomeComprador(e.target.value)} />
         </div>
         <div>
             <button className="" onClick={(e) => atualizarClicou()}>
